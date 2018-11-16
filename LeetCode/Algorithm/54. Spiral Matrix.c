55dd36f68a067ad4a26de417b11d619586af327c
Yu Cao <yu_cao@zju.edu.cn>
2018-11-16 21:19:14 +0800

    LeetCode Train

--- LeetCode/Algorithm/54. Spiral Matrix.c
+++ LeetCode/Algorithm/54. Spiral Matrix.c
@@ -5,19 +5,55 @@ int* spiralOrder(int** matrix, int matrixRowSize, int matrixColSize) {
     int i = 0, j = 0;
     int rowTemp = matrixRowSize - 1;
     int rowTemp1 = 0;//第0行
-    
+
     int colTemp = matrixColSize - 1;
     int colTemp1 = 0;//第0列
-    
-    for(i = colTemp1; i < matrixRowSize; i++)
+
+    int* ans = (int*)malloc((matrixColSize * matrixRowSize +1) * sizeof(int));
+    long count=0;
+
+    for(;;)
     {
-        printf("%d",matrix[rowTemp1][i]);//打印最上面
+        //Print the first line
+        for(j = colTemp1; j <= colTemp; j++)
+        {
+            ans[count] = matrix[rowTemp1][j];
+            count++;
+        }
+        rowTemp1++;
+        if(rowTemp < rowTemp1 || colTemp < colTemp1)
+            break;
+
+        //Print the right line
+        for(i = rowTemp1; i <= rowTemp; i++)
+        {
+            ans[count] = matrix[i][colTemp];
+            count++;
+        }
+        colTemp--;
+        if(rowTemp < rowTemp1 || colTemp < colTemp1)
+            break;
+
+        //Print the bottom line
+        for(j = colTemp;j >= colTemp1;j-- )
+        {
+            ans[count] = matrix[rowTemp][j];
+            count++;
+        }
+        rowTemp--;
+        if(rowTemp < rowTemp1 || colTemp < colTemp1)
+            break;
+
+        //Print the left line
+        for(i = rowTemp;i >= rowTemp1;i--)
+        {
+            ans[count] = matrix[i][colTemp1];
+            count++;
+        }
+        colTemp1++;
+
+        if(rowTemp < rowTemp1 || colTemp < colTemp1)
+            break;
     }
-    rowTemp1++;
-    for(j = rowTemp1; j < matrixColSize; j++)
-    {
-        printf("%d",matrix[j][colTemp]);//打印最右边
-    }
-    for(i--; )
-    
+    return ans;
 }
