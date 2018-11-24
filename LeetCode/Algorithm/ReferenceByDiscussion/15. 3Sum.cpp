48b5f3699f053f7ec5f4a59de98d70123b29d551
CaoYu <yu_cao@zju.edu.cn>
2018-11-25 00:37:22 +0800

    Get help from discussion

---
+++ LeetCode/Algorithm/ReferenceByDiscussion/15. 3Sum.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+
+        //i is the leftist (the main to hold)
+        //l is the left of the rest, r is the right of the rest
+        for (auto i = 0; i < nums.size(); i++)
+        {
+            if ((i > 0) && (nums[i] == nums[i - 1]))
+                continue;
+
+            auto l = i + 1;
+            auto r = nums.size() - 1;
+            while (l < r)
+            {
+                int s = nums[i] + nums[l] + nums[r];
+
+                //Adjust the result compare to the zero
+                if (s > 0)
+                    r--;
+                else if (s < 0)
+                    l++;
+                else
+                {
+                    res.push_back(vector<int>{nums[i], nums[l], nums[r]});
+
+                    //Case of [-4,2,2,2,2,3], otherwise will cause repeat
+                    while (nums[l] == nums[l + 1])
+                        l++;
+                    while (nums[r] == nums[r - 1])
+                        r--;
+
+                    //Not directly break; for the case of [-4,-1,0,0,1]
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+};
