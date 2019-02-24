b6d64a4e011b783f73990674213fc428ea1e871c
CaoYu <yu_cao@zju.edu.cn>
2019-02-24 15:03:04 +0800

    回溯法经典题

--- LeetCode/Algorithm/39. Combination Sum.cpp
+++ LeetCode/Algorithm/39. Combination Sum.cpp
@@ -1,92 +1,27 @@
-#include <vector>
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-class Solution
-{
+//回溯法，先排好序使得回溯法可以正常运行，然后自己定义一个函数进行回溯（DFS策略）
+class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
-    {
-        sort(candidates.begin(), candidates.end());
+	vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+	{
+		sort(candidates.begin(), candidates.end());
+		vector<vector<int>> res;
+		vector<int> combination;
+		combinationSum(candidates, target, res, combination, 0);
+		return res;
+	}
 
-        vector<vector<int>> ans;
-        int num = 0;
-
-        int count[candidates.size()];//The count of the answer
-
-        //TO BE USED
-        int delta[candidates.size()-1];//The delta value of right element
-
-        memset(count, 0, sizeof(int) * candidates.size());
-
-        for (;; count[0]++)
-        {
-            if (count[0] * candidates[0] >= target) break;
-        }
-
-        for (;;)
-        {
-            int flag = false;
-
-            //Calculate the result to compare with target
-            int s = 0;
-            for (int i = 0; i < candidates.size(); i++)
-            {
-                s += count[i] * candidates[i];
-            }
-
-            //Print answer
-            if (s == target)
-            {
-                for (int i = 0; i < candidates.size(); i++)
-                {
-                    for (int j = 0; j < count[i]; j++)
-                    {
-                        ans[num].push_back(candidates[i]);
-                    }
-                }
-            }
-            else if (s < target)
-            {
-                //TODO
-
-
-
-            }
-            else if(s > target)
-            {
-                //TODO
-
-
-            }
-
-
-            //Loop break condition
-            for (int i = 0; i < candidates.size() - 1; i++)
-            {
-                if (count[i] != 0)
-                    flag = true;
-            }
-
-            if (flag == 0)
-                break;
-        }
-
-    }
-
-
+	void combinationSum(vector<int> &candidates, int target, vector<vector<int>> &res, vector<int> &combination, int begin)
+	{
+		if (!target)
+		{
+			res.push_back(combination);//找到一组解
+			return;
+		}
+		for (int i = begin; i != candidates.size() && candidates[i] <= target; ++i)
+		{
+			combination.push_back(candidates[i]);
+			combinationSum(candidates, target - candidates[i], res, combination, i);//不断用vector中的sum和target之间进行比较
+			combination.pop_back();//如果不行就回溯出来
+		}
+	}
 };
-
-int main()
-{
-    Solution ans;
-    vector<int> a = {2, 3, 6, 7};
-    int target = 15;
-
-    vector<vector<int>> judge = ans.combinationSum(a, 15);
-
-    cout << judge.size();
-}
-
